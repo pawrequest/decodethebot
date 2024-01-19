3cd650a5f4e406a0390201933e8a2a449cec18e6
paw <paw@pawrequest.com>
2024-01-19 11:45:02 +0000

    lol

--- src/DecodeTheBot/routers/smth.py
+++ src/DecodeTheBot/routers/smth.py
@@ -1,32 +1,31 @@
 from fastapi import Depends
 from fastui import AnyComponent, FastUI, components as c
 from pawsupport.fastui_suport.fuis import RoutableModel
-from pawsupport.misc import snake_name
+from pawsupport.misc import snake_name, snake_name_s
 from pydantic import BaseModel, Field
 from sqlmodel import Session, select
 
 from DecodeTheBot.core.consts import PAGE_SIZE
 from DecodeTheBot.core.database import get_session
-from DecodeTheBot.models.guru import guru_filter_init
 from DecodeTheBot.models.reddit_thread import RedditThread
 from DecodeTheBot.routers.red import router
 from DecodeTheBot.ui.dtg_ui import dtg_default_page, objects_ui_with
 
-def filter_factory(model, url, placeholder):
+
+def filter_factory(model):
     class SomeFilter(BaseModel):
         tag_name: str = Field(
             json_schema_extra={
-                "search_url": url,
-                "placeholder": placeholder,
+                "search_url": f"/api/forms/search/{snake_name_s(model)}/",
+                "placeholder": f"Filter by {model.__name__}...",
             }
         )
 
-    return SomeFilter()
-
+    return SomeFilter
 
 
 def tag_filter(filter_form_initial, model):
-    filter_model = filter_factory()
+    filter_model = filter_factory(model)
     return c.ModelForm(
         model=filter_model,
         submit_url=".",
@@ -36,11 +35,14 @@ def tag_filter(filter_form_initial, model):
         display_mode="inline",
     )
 
+
 def tag_filter_init(tag_name, tag_model, session, clazz):
     filter_form_initial = {}
     if tag_name:
         tag = session.exec(select(tag_model).where(tag_model.name == tag_name)).one()
+
         statement = select(clazz).where(clazz.gurus.any(tag_model.id == tag.id))
+
         data = session.exec(statement).all()
         filter_form_initial[snake_name(tag_model)] = {"value": tag_name, "label": tag.name}
     else:
@@ -48,11 +50,15 @@ def tag_filter_init(tag_name, tag_model, session, clazz):
     return data, filter_form_initial
 
 
-
-def make_route(clazz, tag_model:RoutableModel):
-    @router.get(f"/{tag_model.rout_prefix()}", response_model=FastUI, response_model_exclude_none=True)
+def make_route(clazz, tag_model: RoutableModel):
+    @router.get(
+        f"/{tag_model.rout_prefix()}", response_model=FastUI, response_model_exclude_none=True
+    )
     def smth_list_view(
-        page: int = 1, tag_name: str | None = None, session: Session = Depends(get_session), data=None
+        page: int = 1,
+        tag_name: str | None = None,
+        session: Session = Depends(get_session),
+        data=None,
     ) -> list[AnyComponent]:
         data, filter_form_initial = tag_filter_init(tag_name, tag_model, session, clazz)
         data.sort(key=lambda x: x.created_datetime, reverse=True)
@@ -70,6 +76,8 @@ def make_route(clazz, tag_model:RoutableModel):
         )
 
     return smth_list_view
+
+
 @router.get("/", response_model=FastUI, response_model_exclude_none=True)
 def smth_list_view(
     page: int = 1, tag_name: str | None = None, session: Session = Depends(get_session), data=None
